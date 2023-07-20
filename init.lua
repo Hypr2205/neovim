d7e84bb11c4ea1b78e9d968d874ab98f35beaf19
CaChienXuu <procango2003@gmail.com>
2023-07-20 18:02:42 +0700

    Change colorscheme to catppuccin

--- init.lua
+++ init.lua
@@ -1,60 +1,48 @@
-require('cachienxu')
+require("cachienxu")
 
-require('rose-pine').setup({
-  --- @usage 'auto'|'main'|'moon'|'dawn'
-  variant = 'auto',
-  --- @usage 'main'|'moon'|'dawn'
-  dark_variant = 'main',
-  bold_vert_split = false,
-  dim_nc_background = false,
-  disable_background = true,
-  disable_float_background = false,
-  disable_italics = false,
-
-  --- @usage string hex value or named color from rosepinetheme.com/palette
-  groups = {
-    background = 'base',
-    background_nc = '_experimental_nc',
-    panel = 'surface',
-    panel_nc = 'base',
-    border = 'highlight_med',
-    comment = 'muted',
-    link = 'iris',
-    punctuation = 'subtle',
-
-    error = 'love',
-    hint = 'iris',
-    info = 'foam',
-    warn = 'gold',
-
-    headings = {
-      h1 = 'iris',
-      h2 = 'foam',
-      h3 = 'rose',
-      h4 = 'gold',
-      h5 = 'pine',
-      h6 = 'foam',
-    }
-    -- or set all headings at once
-    -- headings = 'subtle'
-  },
-
-  -- Whether or not highlight_groups optios should change only only update
-  -- the settings they touch or should reset the entire highlight_group.
-  respect_default_highlight_groups = true,
-
-  -- Change specific vim highlight groups
-  -- https://github.com/rose-pine/neovim/wiki/Recipes
-  highlight_groups = {
-    ColorColumn = { bg = 'rose' },
-
-    -- Blend colours against the "base" background
-    CursorLine = { bg = 'foam', blend = 10 },
-    StatusLine = { fg = 'love', bg = 'love', blend = 10 },
-  }
+require("catppuccin").setup({
+	flavour = "mocha", -- latte, frappe, macchiato, mocha
+	background = { -- :h background
+		light = "latte",
+		dark = "mocha",
+	},
+	transparent_background = true, -- disables setting the background color.
+	show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+	term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+	dim_inactive = {
+		enabled = false, -- dims the background color of inactive window
+		shade = "dark",
+		percentage = 0.15, -- percentage of the shade to apply to the inactive window
+	},
+	no_italic = false, -- Force no italic
+	no_bold = false, -- Force no bold
+	no_underline = false, -- Force no underline
+	styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+		comments = { "italic" }, -- Change the style of comments
+		conditionals = { "italic" },
+		loops = {},
+		functions = {},
+		keywords = {},
+		strings = {},
+		variables = {},
+		numbers = {},
+		booleans = {},
+		properties = {},
+		types = {},
+		operators = {},
+	},
+	color_overrides = {},
+	custom_highlights = {},
+	integrations = {
+		cmp = true,
+		gitsigns = true,
+		nvimtree = true,
+		treesitter = true,
+		notify = false,
+		mini = false,
+		-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+	},
 })
 
--- Set colorscheme after options
-vim.cmd('colorscheme rose-pine')
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- setup must be called before loading
+vim.cmd.colorscheme("catppuccin")
